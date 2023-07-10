016ec9e8901ed2e594b4cb8277d56d4e00d14fc0
Akhil Jayan <akhil.jayan1@ibm.com>
2023-07-11 00:21:07 +0530

    changing location for cos to a valid value

--- course-content/cos/main.tf
+++ course-content/cos/main.tf
@@ -8,6 +8,6 @@ resource "ibm_resource_instance" "cloud-object-storage" {
   name              = var.cos_name
   service           = "cloud-object-storage"
   plan              = var.cos_plan
-  location          = var.location
+  location          = "global"
   resource_group_id = var.course_rg_id
 }
